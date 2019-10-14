c9f49832fa15bae7ee6f40df473565c01320a32a
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-10-14 16:20:04 -0400

    adds reset and animate button functionality

--- AnimationsTwo-Lab/Controllers/ViewController.swift
+++ AnimationsTwo-Lab/Controllers/ViewController.swift
@@ -10,16 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     //MARK: - Properties
+    //MARK: Images
     lazy var linearBall = BallImage()
     lazy var easeInBall = BallImage()
     lazy var easeOutBall = BallImage()
     lazy var easeInOutBall = BallImage()
     
-    var linearBallOrigin = CGPoint()
-    var easeInBallOrigin = CGPoint()
-    var easeOutBallOrigin = CGPoint()
-    var easeInOutBallOrigin = CGPoint()
-    
+    //MARK: Buttons
     lazy var linearButton: UIButton = {
         let button = UIButton()
         button.setTitle("Linear", for: .normal)
@@ -53,6 +50,7 @@ class ViewController: UIViewController {
         button.setTitle("Reset", for: .normal)
         button.setTitleColor(.blue, for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(resetButtonPressed), for: .touchUpInside)
         return button
     }()
     lazy var animateButton: UIButton = {
@@ -60,6 +58,7 @@ class ViewController: UIViewController {
         button.setTitle("Animate", for: .normal)
         button.setTitleColor(.blue, for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(animateButtonPressed), for: .touchUpInside)
         return button
     }()
     //MARK: - Constraint Functions
@@ -70,16 +69,9 @@ class ViewController: UIViewController {
         setupEaseInOutButton()
         
         setUp(ball: linearBall, to: linearButton)
-        linearBallOrigin = linearBall.center
-        
         setUp(ball: easeInBall, to: easeInButton)
-        easeInBallOrigin = easeInBall.center
-        
         setUp(ball: easeOutBall, to: easeOutButton)
-        easeOutBallOrigin = easeOutBall.center
-        
         setUp(ball: easeInOutBall, to: easeInOutButton)
-        easeInOutBallOrigin = easeInBall.center
         
         setupResetButton()
         setupAnimateButton()
@@ -121,6 +113,29 @@ class ViewController: UIViewController {
         view.addSubview(animateButton)
         animateButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor,constant: -20).isActive = true
         animateButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    }
+    private func animate(ball: UIView,animateOption: UIView.AnimationOptions) {
+        
+            UIView.animate(withDuration: 3, delay: 0, options: animateOption, animations: {
+                ball.center = CGPoint(x: ball.center.x, y: ball.center.y + 600)
+            }, completion: nil)
+        
+    }
+    //MARK: - @objc Functions
+    @objc func resetButtonPressed() {
+        setUp(ball: linearBall, to: linearButton)
+        setUp(ball: easeInBall, to: easeInButton)
+        setUp(ball: easeOutBall, to: easeOutButton)
+        setUp(ball: easeInOutBall, to: easeInOutButton)
+        view.layoutIfNeeded()
+    }
+    @objc func animateButtonPressed() {
+       
+            self.animate(ball: self.linearBall, animateOption: .curveLinear)
+            self.animate(ball: self.easeInBall, animateOption: .curveEaseIn)
+            self.animate(ball: self.easeOutBall, animateOption: .curveEaseOut)
+            self.animate(ball: self.easeInOutBall, animateOption: .curveEaseInOut)
+     
     }
 
     //MARK: - LifeCycle
